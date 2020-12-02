8c0528ef147feeba2d01e68a18fdb38a23da48f4
dupham1998 <ngocdu12a1@gmail.com>
2020-12-02 21:25:35 +0700

    test commit source tree

--- app/src/main/java/com/example/filemanager/activities/MainActivity.java
+++ app/src/main/java/com/example/filemanager/activities/MainActivity.java
@@ -20,7 +20,6 @@ import android.widget.ImageView;
 import android.widget.Toast;
 
 import com.example.filemanager.R;
-import com.example.filemanager.fragments.SearchFragment;
 import com.example.filemanager.fragments.ShowFileFragment;
 
 import java.util.List;
@@ -62,16 +61,10 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     @Override
     public void onClick(View v){
         if(v.getId() == R.id.imv_folder){
-            FragmentManager fm = getSupportFragmentManager();
-            FragmentTransaction f = fm.beginTransaction();
-            f.replace(R.id.fl_activity, new ShowFileFragment());
-            f.commit();
+            Toast.makeText(this, "click folder", Toast.LENGTH_SHORT).show();
         }
         else if(v.getId() == R.id.imv_search){
-            FragmentManager fm = getSupportFragmentManager();
-            FragmentTransaction f = fm.beginTransaction();
-            f.replace(R.id.fl_activity, new SearchFragment());
-            f.commit();
+            Toast.makeText(this, "click search", Toast.LENGTH_SHORT).show();
         }
     }
 
